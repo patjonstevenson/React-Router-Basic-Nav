da7f041a82bdf090b8fe3b4cf136b23504c93619
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-16 14:47:35 -0400

    wrapper <App /> in Router in index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
